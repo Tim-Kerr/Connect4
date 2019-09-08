7676b0aed4087991bcb129182d1c340fe8523671
Tim <mitoscalendar@gmail.com>
2019-09-07 20:43:13 -0600

    grid: Implemented game win logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -22,9 +22,53 @@ const Game: React.FC = () => {
     setGameEnd(true);
   }
 
-  const detectWin = (): Color => {
+  /**
+   * Detects if a player has won the game
+   * 
+   * Checks the surrounding board relative to the last piece played.
+   * 
+   * @param color: The last color played
+   * @param column: The column last played
+   */
+  const didWin = (color: Color, column: number): boolean => {
+    // Check vertical win
+    if (check(x => x, y => y + 1, color, column) + check(x => x, y => y - 1, color, column) >= 3) {
+      return true;
+    }
 
-    return Color.NONE;
+    // Check horizontal win
+    if (check(x => x + 1, y => y, color, column) + check(x => x - 1, y => y, color, column) >= 3) {
+      return true;
+    }
+
+    // Check up and to the right diagonal 
+    if (check(x => x + 1, y => y + 1, color, column) + check(x => x - 1, y => y - 1, color, column) >= 3) {
+      return true;
+    }
+
+    // Check down and to the left diagonal
+    if (check(x => x - 1, y => y + 1, color, column) + check(x => x + 1, y => y - 1, color, column) >= 3) {
+      return true;
+    }
+
+    return false;
+  }
+
+  const check = (xTraveralFunc: (x: number) => number, yTraversalFunc: (y: number) => number, color: Color, column: number): number => {
+    let count = 0;
+    let x = column;
+    let y = board[x].length - 1;
+    for (let i = 0; i < 3; i++) {
+      x = xTraveralFunc(x);
+      y = yTraversalFunc(y);
+
+      if (x >= board.length || x < 0) break;
+      if (y >= board[x].length || y < 0) break;
+
+      if (board[x][y] === color) count++;
+    }
+
+    return count;
   }
 
   return (
@@ -63,18 +107,23 @@ const Game: React.FC = () => {
             setTurnCount(0);
             setGameEnd(false);
             setGameStart(false);
+            setWinner(Color.NONE);
           }}>Ok</button>
         </Modal>}
 
-      {gameStart && !gameEnd &&
+      {gameStart &&
         <Board
           board={board}
           onTurnFinish={(column: number) => {
             board[column].push(turn); // Push the gamepiece onto the column
             setBoard([...board]);
-            detectWin();
             setTurn((turn === Color.RED) ? Color.BLACK : Color.RED);
             setTurnCount(turnCount + 1);
+
+            if (didWin(turn, column)) {
+              setWinner(turn);
+              setGameEnd(true);
+            }
           }}
           turn={turn} />}
     </div>
